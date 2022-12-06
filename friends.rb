cba24859e4df0dde8aef0c45111f34235f2baf9a
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2022-12-06 14:07:28 -0700

    Initial commit

---
+++ friends.rb
@@ -0,0 +1,7 @@
+arrays = ['Andrew', 'Ty', 'Sophie', 'Tryssie', 'Kailey']
+puts arrays[2] 
+puts arrays[4]
+puts arrays[0]
+places = ['seattle', 'Sao Paulo', 'Eugene, Oregon', 'Nigeria', 'Maine']
+places.pop 
+    puts places
